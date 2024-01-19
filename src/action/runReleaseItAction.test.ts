8f8c65943849be5127c0c3768a5f26abda839be9
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-01-19 10:28:12 -0500

    lint

--- src/action/runReleaseItAction.test.ts
+++ src/action/runReleaseItAction.test.ts
@@ -1,5 +1,5 @@
 import * as github from "@actions/github";
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
 import { runReleaseItAction } from "./runReleaseItAction.js";
 
