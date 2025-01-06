9ea94dd8dc753d4623927acbc234f91b9ae9a6df
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-01-06 17:29:20 -0500

    feat: add release-it-args option (#392)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #390
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/release-it-action/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/release-it-action/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Adds a `release-it-args` option.
    
    💖

--- src/steps/runReleaseIt.ts
+++ src/steps/runReleaseIt.ts
@@ -3,10 +3,12 @@ import * as core from "@actions/core";
 import { $$ } from "../execa.js";
 import { tryCatchInfoAction } from "../tryCatchInfoAction.js";
 
-export async function runReleaseIt() {
+export async function runReleaseIt(releaseItArgs?: string) {
+	const args = releaseItArgs ? ` ${releaseItArgs}` : "";
+
 	await tryCatchInfoAction("running release-it", async () => {
 		try {
-			const { exitCode, stderr } = await $$`npx release-it --verbose`;
+			const { exitCode, stderr } = await $$`npx release-it --verbose${args}`;
 			if (exitCode || stderr) {
 				throw new Error(stderr || `Exit code ${exitCode.toString()}.`);
 			}
